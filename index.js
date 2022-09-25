d4c15bc20b377767e7c2410f6e3f87d7f5e947d2
HaroldZS <haroldzs@gmail.com>
2022-09-24 20:10:35 -0400

    recuperado y listado archivo json

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+let postsElements = '';
+
 fetch('csvjson.json')
     .then(response => {
         return response.json();
     })
     .then(data => {
-        console.log(data);
+        for (let i=0; i < data.length; i++){
+            postsElements += `<li>${data[i].Zone} - ${data[i].Store}
+            - ${data[i]["Salesman name"]} - ${data[i].Date}</li>`;
+        }
+        document.getElementById('posts').innerHTML = postsElements;
     })
